46cb647d884f3eb170b750146372566259ffc93b
Catty <ducvu73@gmail.com>
2022-11-22 15:02:32 +0700

    Update Bai 4 - Ket Hop Menu Cho Ca 3 Chuong Trinh - LAB4.c
    Pls use DevC++ to run this code

--- Bai 4 - Ket Hop Menu Cho Ca 3 Chuong Trinh - LAB4.c
+++ Bai 4 - Ket Hop Menu Cho Ca 3 Chuong Trinh - LAB4.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 //Khai báo biến string cho menu
 char mn[]="	||	||\n		||	||\n	+===Menu Chuong Trinh===+";
 char mn1[]="1.Tinh trung binh cong";
